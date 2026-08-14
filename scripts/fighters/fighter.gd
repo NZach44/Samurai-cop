ee3418ec122d390b32f67a075e1ec80de20fb6bd
NZach <greenteam1221@outlook.com>
2026-08-14 13:52:22 +0300

    Add basic player fighter jump

--- scripts/fighters/fighter.gd
+++ scripts/fighters/fighter.gd
@@ -1,14 +1,16 @@
 extends CharacterBody2D
 
 @export var move_speed: float = 240.0
+@export var jump_velocity: float = 600.0
 
 
 func _physics_process(delta: float) -> void:
 	var move_direction: float = Input.get_axis("p1_left", "p1_right")
 	velocity.x = move_direction * move_speed
 
-	# Gravity keeps the fighter resting on arena collision without adding jumping.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+	elif Input.is_action_just_pressed("p1_jump"):
+		velocity.y = -jump_velocity
 
 	move_and_slide()
